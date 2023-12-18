4d69c51bb0cd6d6fb6e540d18b1647d407ca8a38
jscodin <jascode9@gmail.com>
2023-12-18 23:40:38 +0000

    add updated files, and updated install list

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -27,7 +27,7 @@ require("lazy").setup({
     version = false, -- always use the latest git commit
     -- version = "*", -- try installing the latest stable version for plugins that support semver
   },
-  install = { colorscheme = { "tokyonight", "habamax", "ayu", "onedark" } },
+  install = { colorscheme = { "tokyonight", "habamax", "ayu", "onedark", "kanagawa" } },
   checker = { enabled = true }, -- automatically check for plugin updates
   performance = {
     rtp = {
---
+++ .config/nvim/lua/plugins/kanagawa.lua
@@ -0,0 +1,27 @@
+return {
+
+  {
+    "rebelot/kanagawa.nvim",
+
+    -- Default options:
+    config = function()
+      require('kanagawa').setup {
+        compile = false, -- enable compiling the colorscheme
+        undercurl = true, -- enable undercurls
+        commentStyle = { italic = true },
+        functionStyle = {},
+        keywordStyle = { italic = true },
+        statementStyle = { bold = true },
+        typeStyle = {},
+        transparent = false, -- do not set background color
+        dimInactive = false, -- dim inactive window `:h hl-NormalNC`
+        terminalColors = true, -- define vim.g.terminal_color_{0,17}
+        colors = {           -- add/modify theme and palette colors
+          palette = {},
+          theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
+        },
+      }
+      require("kanagawa").load("wave")
+    end,
+  },
+}
